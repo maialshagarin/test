6fd280223805a12776249f47db0ec093b2b0a876
maialshagarin <55940800+maialshagarin@users.noreply.github.com>
2022-01-08 22:29:29 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,163 @@
+1. what's  "this" keyword in javascript?
+The JavaScript this keyword refers to the object it belongs to. It has different values depending on where it is used: In a method, this refers to the owner object. Alone, this refers to the global object.
+-------------------------------------------------------
+2. What is the difference between " ===" and "=="?
+== is used for comparison between two variables irrespective of the datatype of variable. === is used for comparision between two variables but this will check strict type, which means it will check datatype and compare two values
+-------------------------------------------------------
+
+3. What is HTML ?
+HTML is the standard markup language for creating Web pages.
+
+What is HTML?
+HTML stands for Hyper Text Markup Language
+HTML is the standard markup language for creating Web pages
+HTML describes the structure of a Web page
+HTML consists of a series of elements
+HTML elements tell the browser how to display the content
+HTML elements label pieces of content such as "this is a heading", "this is a paragraph", "this is a link", etc
+---------------------------------------------------------
+4. What is Object Destructuring ?
+The object destructuring is a useful JavaScript feature to extract properties from objects and bind them to variables. What's better, object destructuring can extract multiple properties in one statement, can access properties from nested objects, and can set a default value if the property doesn't exist. 
+or
+The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+
+-----------------------------------------------------------
+
+5. What are javascript Data types?
+strings, numbers, booleans, undefined, and null , object , array 
+
+-----------------------------------------------------------
+
+6. What does SQL stand for? 
+SQL (Structured Query Language) is a descriptive computer language designed for updating, retrieving, and calculating data in table-based databases
+
+-----------------------------------------------------------
+7. Explain the term CLI?
+https://techterms.com/definition/command_line_interface
+Abbreviated as CLI, a Command Line Interface connects a user to a computer program or operating system. Through the CLI, users interact with a system or application by typing in text (commands). The command is typed on a specific line following a visual prompt from the computer.
+
+-----------------------------------------------------------
+8. Compare NoSQL & RDBMS?
+https://www.geeksforgeeks.org/difference-between-sql-and-nosql/
+
+-----------------------------------------------------------
+9. What are closures ?
+A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).
+losure means that an inner function always has access to the vars and parameters of its outer function, even after the outer function has returned.
+
+-----------------------------------------------------------
+10. What is DOM?
+The DOM (Document Object Model) is an API that represents and interacts with any HTML or XML document. The DOM is a document model loaded in the browser and representing the document as a node tree, where each node represents part of the document (e.g. an element, text string, or comment).
+Nodes can be created, moved and changed. Event listeners can be added to nodes and triggered on occurrence of a given event.
+
+-----------------------------------------------------------
+11. What is AJAX?
+AJAX = Asynchronous JavaScript And XML.
+
+AJAX is not a programming language.
+
+AJAX just uses a combination of:
+
+A browser built-in XMLHttpRequest object (to request data from a web server)
+JavaScript and HTML DOM (to display or use the data)
+
+-----------------------------------------------------------
+12. What are the new features in ES6 or ECMAscript2015 ?
+
+New Features in ES6 : The let keyword ,The const keyword , Arrow Functions ,For/of, Map Objects ,Set Objects ,Classes ,Promises ,Symbol ,Default Parameters ,Function Rest Parameter ,String.includes() ,String.startsWith() ,String.endsWith() ,Array.from(), Array keys(),Array find() ,Array findIndex() ,New Math Methods ,New Number Properties ,New Number Methods ,New Global Methods ,Iterables Object.entries ,JavaScript Modules
+
+-----------------------------------------------------------
+13. What is the difference between a block-level element and an inline element?
+
+A block-level element always starts on a new line. A block-level element always takes up the full width available (stretches out to the left and right as far as it can). A block level element has a top and a bottom margin, whereas an inline element does not. The <div> element is a block-level element.
+
+-----------------------------------------------------------
+14. What are two types of Web Storage in HTML5?
+localStorage.
+SessionStorage.
+
+-----------------------------------------------------------
+15. What is the importance of the <head> tag?
+
+Significance of HTML <head> tag: The head tag in HTML is used to contain the metadata or information related to the document. It holds some of the most important tags like <title> , <meta> , and <link>.Sep 1, 2021
+
+-----------------------------------------------------------
+16. What are Promises?
+https://www.geeksforgeeks.org/javascript-promises/
+Promises are used to handle asynchronous operations in JavaScript. They are easy to manage when dealing with multiple asynchronous operations where callbacks can create callback hell leading to unmanageable code. 
+
+-----------------------------------------------------------
+17. What are the different lifecycle methods in React?
+Each component in React has a lifecycle which you can monitor and manipulate during its three main phases.
+The three phases are: Mounting, Updating, and Unmounting.
+https://www.w3schools.com/react/react_lifecycle.asp
+
+-----------------------------------------------------------
+18. Why use Node.js?
+https://www.simplilearn.com/tutorials/nodejs-tutorial/nodejs-interview-question
+Node.js makes building scalable network programs easy. Some of its advantages include:
+
+It is generally fast
+It rarely blocks
+It offers a unified programming language and data type
+Everything is asynchronous 
+It yields great concurrency
+
+https://www.geeksforgeeks.org/why-node-js/
+-----------------------------------------------------------
+19. How does Node.js work?
+
+-----------------------------------------------------------
+20. why is Node.js Single-threaded?
+
+-----------------------------------------------------------
+21. Explain the difference between frontend and backend development? 
+
+Front end development is programming which focuses on the visual elements of a website or app that a user will interact with (the client side). Back end development focuses on the side of a website users can't see (the server side)
+
+-----------------------------------------------------------
+22. What are the two types of API functions in Node.js?
+ Asynchronous, non-blocking functions. Synchronous, blocking functions.
+
+-----------------------------------------------------------
+23. What is the package.json file?
+ the heart of any Node project. It records important metadata about a project which is required before publishing to NPM, and also defines functional attributes of a project that npm uses to install dependencies, run scripts, and identify the entry point to our package.
+
+-----------------------------------------------------------
+24. What are the different types of HTTP requests?
+ https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+ The most common types of request methods are GET and POST but there are many others, including HEAD, PUT, DELETE, CONNECT, and OPTIONS ,patch. GET and POST are widely supported while support for other methods is sometimes limited but expanding.
+
+-----------------------------------------------------------
+25. What is the Express.js package?
+https://www.tutorialsteacher.com/nodejs/expressjs
+Express.js is a web application framework for Node.js. It provides various features that make web application development fast and easy which otherwise takes more time using only Node.js.
+
+-----------------------------------------------------------
+26. What is npm? What is the main functionality of npm?
+
+-----------------------------------------------------------
+27. What Is CSS?
+
+-----------------------------------------------------------
+28.  What is the Box model in CSS? Which CSS properties are a part of it?
+
+-----------------------------------------------------------
+29. What are the different types of Selectors in CSS?
+https://www.w3schools.com/css/css_selectors.asp
+-----------------------------------------------------------
+30.  Does margin-top or margin-bottom have an effect on inline elements?
+
+-----------------------------------------------------------
+31. Differentiate between Real DOM and Virtual DOM
+
+-----------------------------------------------------------
+32. What is JSX?
+
+JSX stands for JavaScript XML. It is simply a syntax extension of JavaScript. It allows us to directly write HTML in React (within JavaScript code). It is easy to create a template using JSX in React, but it is not a simple template language instead it comes with the full power of JavaScript.
+It is faster than normal JavaScript as it performs optimizations while translating to regular JavaScript. Instead of separating the markup and logic in separated files, React uses components for this purpose. We will learn about components in detail in further articles. 
+-----------------------------------------------------------
+33. what is API?
+API is the acronym for Application Programming Interface, which is a software intermediary that allows two applications to talk to each other. Each time you use an app like Facebook, send an instant message, or check the weather on your phone, you’re using an API.
+
+-----------------------------------------------------------
